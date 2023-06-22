359314e2bedae53ceb25b984fd292e911c6d08a9
Dariionok <115529102+DariiaMurakaeva@users.noreply.github.com>
2023-06-22 20:51:09 +0300

    Update all.js

--- javascripts/all.js
+++ javascripts/all.js
@@ -2,4 +2,66 @@ $(document).ready(function(){
     $(".pink, .green, .blue, .orange").click(function(){
         $(this).toggleClass("selected")
     });
+
+    // Смена цвета у товаров
+    $(".white").click(function(){
+        $(".white").toggleClass("chosen");
+        $(".black").toggleClass("chosen");
+        if ($(".tovar").hasClass("tovar-black")) {
+        $(".tovar").removeClass("tovar-black");
+    }
     });
+    
+    $(".black").click(function(){
+        $(".white").toggleClass("chosen");
+    $(".black").toggleClass("chosen");
+    $(".tovar").toggleClass("tovar-black");
+    });
+
+    // Добавление в корзину
+    let itemsInCart = 0;
+    
+    $(".icon").click(function () {
+        $(".cart_link h2").text($(".cart_link").text().replace(itemsInCart.toString(), (itemsInCart+ 1).toString()));
+        itemsInCart += 1;
+    });
+
+    $('.bag-ico').click( function () {
+        let rez = $('<div class="popup1">Добавлено!</div>');
+        $('#added-bag').append(rez);
+        setTimeout( function() { rez.remove(); }, 500);
+    });
+
+    // Регистрация на мероприятия
+    $(".poster-lectures, .poster-karaoke").click(function(){
+        $(".forma").removeClass("none")
+        });
+    $(".cross").click(function(){
+        $(".forma").addClass("none")
+        });
+
+        $(".button-reg").click(function(){
+            $(".forma").addClass("none")
+            $(".forma-done").removeClass("none")
+            });
+            $(".cross-done").click(function(){
+                $(".forma-done").addClass("none")
+                });
+    // Подписка на рассылку, привидение
+    $(".mailing-btn").click(function(){
+        $(".ghost-afisha").removeClass("none");
+        $(".ghost-afisha").addClass("fly");
+        setTimeout(function(){
+            $('.ghost-afisha').addClass("none");
+        }, 1500);
+    });
+
+    // Покупка билета, привидение
+    $(".btn-ticket").click(function(){
+        $(".ghost-ticket").removeClass("none");
+        $(".ghost-ticket").addClass("fly-2");
+        setTimeout(function(){
+            $('.ghost-ticket').addClass("none");
+        }, 1500);
+    });
+});
